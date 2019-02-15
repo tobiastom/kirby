a5da1812b8623cedacf3b85cb7bdf1ad2870bddb
Nico Hoffmann <on-github@nhoffmann.com>
2019-02-15 19:09:47 +0100

    Move to API library

--- panel/src/api/site.js
+++ panel/src/api/site.js
@@ -1,38 +1,17 @@
 import Vue from "vue";
-import api from "./api.js";
+import api from "@getkirby/api-js";
 
 export default {
-  get(query) {
-    return api.get("site", query);
-  },
-  update(data) {
-    return api.post("site", data);
-  },
-  title(title) {
-    return api.patch("site/title", { title: title });
-  },
-  options() {
-    return api.get("site", {select: "options"}).then(site => {
-      const options = site.options;
-      let result    = [];
-
-      result.push({
-        click: "rename",
-        icon: "title",
-        text: Vue.i18n.translate("rename"),
-        disabled: !options.changeTitle
-      });
-
-      return result;
+  async options() {
+    const site = await api.get("site", { select: "options" });
+    const options = site.options;
+    let result = [];
+    result.push({
+      click: "rename",
+      icon: "title",
+      text: Vue.i18n.translate("rename"),
+      disabled: !options.changeTitle
     });
-  },
-  children(query) {
-    return api.post("site/children/search", query);
-  },
-  blueprint() {
-    return api.get("site/blueprint");
-  },
-  blueprints() {
-    return api.get("site/blueprints");
+    return result;
   }
 };
--- panel/src/config/api.js
+++ panel/src/config/api.js
@@ -1,9 +1,23 @@
 import Vue from "vue";
-import Api from "../api/api.js";
+import Api from "@getkirby/api-js";
 import config from "./config.js";
 import store from "@/store/store.js";
 
+import files from "@/api/files.js";
+import pages from "@/api/pages.js";
+import roles from "@/api/roles.js";
+import site from "@/api/site.js";
+import users from "@/api/users.js";
+
 Api.config.endpoint = config.api;
+Api.config.csrf     = window.panel.csrf;
+
+Api.config.onRequest = (options) => {
+  if (store.state.languages.current) {
+    options.headers["x-language"] = store.state.languages.current.code;
+  }
+  return options;
+};
 
 Api.config.onStart = () => {
   store.dispatch("isLoading", true);
@@ -31,5 +45,12 @@ Api.config.onSuccess = () => {
   clearInterval(ping);
   ping = setInterval(Api.auth.user, 5 * 60 * 1000);
 };
+
+// Add custom endpoint methods
+Api.files = Object.assign(Api.files, files);
+Api.pages = Object.assign(Api.pages, pages);
+Api.roles = Object.assign(Api.roles, roles);
+Api.site  = Object.assign(Api.site, site);
+Api.users = Object.assign(Api.users, users);
 
 Vue.prototype.$api = Api;
